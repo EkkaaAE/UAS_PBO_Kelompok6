1bbdb97376d9bbb36f3d91fabe6baba2afb619d3
Ekaa Prass <105110665+EkkaaAE@users.noreply.github.com>
2023-08-06 18:53:45 +0700

    Update DataHP.java

--- UAS_PBO_Kelompok6/src/Nomor1UAS/DataHP.java
+++ UAS_PBO_Kelompok6/src/Nomor1UAS/DataHP.java
@@ -11,9 +11,9 @@ package Nomor1UAS;
 public class DataHP implements Pajak{
     String vendor;
     String tipe;
-    double harga;
+    int harga;
 
-    public DataHP(String vendor, String tipe, double harga) {
+    public DataHP(String vendor, String tipe, int harga) {
         this.vendor = vendor;
         this.tipe = tipe;
         this.harga = harga;
@@ -35,26 +35,26 @@ public class DataHP implements Pajak{
     }
     
     public static void main(String[] args) {
-        DataHP hp1 = new DataHP("Samsung", "A 51", 5000000);
+        DataHP hp1 = new DataHP("Samsung", "A 51",5000000);
         DataHP hp2 = new DataHP("Oppo", "Reno 5", 4400000);
         DataHP hp3 = new DataHP("Xiaomi", "A1", 4100000);
 
         System.out.println("hp ke-1:");
         System.out.println("Vendor: " + hp1.vendor);
         System.out.println("Tipe: " + hp1.tipe);
-        System.out.println("Harga: " + hp1.harga);
-        System.out.println("Harga setelah ditambahkan PAJAK: " + hp1.hitungPajak());
+        System.out.println("Harga: Rp " + hp1.harga);
+        System.out.println("Harga setelah ditambahkan PAJAK: Rp " + hp1.hitungPajak());
 
         System.out.println("\nhp ke-2:");
         System.out.println("Vendor: " + hp2.vendor);
         System.out.println("Tipe: " + hp2.tipe);
-        System.out.println("Harga: " + hp2.harga);
-        System.out.println("Harga setelah ditambahkan PAJAK: " + hp2.hitungPajak());
+        System.out.println("Harga: Rp " + hp2.harga);
+        System.out.println("Harga setelah ditambahkan PAJAK: Rp " + hp2.hitungPajak());
 
         System.out.println("\nhp ke-3:");
         System.out.println("Vendor: " + hp3.vendor);
         System.out.println("Tipe: " + hp3.tipe);
-        System.out.println("Harga: " + hp3.harga);
-        System.out.println("Harga setelah ditambahkan PAJAK: " + hp3.hitungPajak());
+        System.out.println("Harga: Rp " + hp3.harga);
+        System.out.println("Harga setelah ditambahkan PAJAK: Rp " + hp3.hitungPajak());
     }
 }
